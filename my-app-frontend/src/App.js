0ab96ae0ca38e26f40d04bb3535f8dbac98e3382
Roli <rroycehg@gmail.com>
2021-08-04 17:59:54 -0400

    8/4/2021

--- my-app-frontend/src/App.js
+++ my-app-frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, { useState} from 'react';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
--- my-app-frontend/src/components/Account.js
+++ my-app-frontend/src/components/Account.js
@@ -1,33 +1,59 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../App.css';
 
 function Account() {
-
+  const [name, setName] = useState("")
+  const [userName, setUserName] = useState("")
+  const [companyName, setCompanyName] = useState("")
+  const [emailAddress, setEmailAddress] = useState("")
+  const [password, setPassword] = useState("")
+  
+useEffect(() => {
+  fetch()
+}, [])
 
   const handleDelete = (userId) =>{
     if (window.confirm("Are you sure you want to delete this account?"))
     console.log('account deleted')
   }
 
+  // const handleClick = () => {
+  //   PATCH
+  // }
 
   return(
     <>
       <h2 id = 'header'>Account Information</h2>
       <ul className = 'account-info' >
         <li className = 'account-list'>Name:
-          <span className = 'change-button'>Change</span>
+          <span className = 'change-button' 
+          value=""
+          onChange=""
+          onClick={() => console.log("Im clicked")}>Change</span>
         </li>
         <li className = 'account-list'>Username:
-          <span className = 'change-button'>Change</span>
+          <span className = 'change-button'
+          value=""
+          onChange=""
+          onClick={() => console.log("Im clicked")}>Change</span>
         </li>
         <li className = 'account-list'>Company Name:
-          <span className = 'change-button'>Change</span>
+          <span className = 'change-button' 
+          value=""
+          onChange=""
+          onClick={() => console.log("Im clicked")}>Change</span>
         </li>
         <li className = 'account-list'>Email Address:
-          <span className = 'change-button'>Change</span>
+          <span className = 'change-button' 
+          value=""
+          onChange=""
+          onClick={() => console.log("Im clicked")}>Change</span>
         </li>
         <li className = 'account-list'>Password:
-          <span className = 'change-button'>Change</span>
+          <span className = 'change-button'
+          value=""
+          onChange=""
+          onClick={() => console.log("Im clicked")}>Change</span>
         </li>
       </ul>
       <button onClick = {handleDelete}>Delete Account</button>
