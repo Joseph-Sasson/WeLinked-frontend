2c28dc0c800da04831cd4091a7527d324121c030
Joseph <josephsasson1@gmail.com>
2021-08-05 12:21:39 -0400

    add update to account and modify some css

--- my-app-frontend/src/components/Account.js
+++ my-app-frontend/src/components/Account.js
@@ -6,7 +6,7 @@ function Account({user, setLogin}) {
   const [userForm, setUserForm] = useState({
     name: user.name,
     username: user.username,
-    company: user.company_id,
+    company_id: user.company_id,
     email: user.email,
     password: user.password
   })
@@ -59,8 +59,8 @@ function Account({user, setLogin}) {
         <li className = 'account-list'>Company Name:
         <input 
             type = 'text'
-            name = 'company'
-            value = {userForm.company}
+            name = 'company_id'
+            value = {userForm.company_id}
             onChange = {handleChange}
           />
         </li>
